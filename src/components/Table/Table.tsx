085bc9622ba5934d15a6003bc24cb36f44f6872e
1sahinomer1 <1sahinomer1@gmail.com>
2023-12-24 16:40:27 +0300

    feat : add loading check

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -18,6 +18,7 @@ const Table = ({
   page,
   isPagination,
   totalResults,
+  isLoading,
   handleRowClick,
 }: TableProps) => {
   const memoizedData = useMemo(() => data, [data]);
@@ -35,35 +36,40 @@ const Table = ({
 
   return (
     <div className="table-container">
-      <table>
-        <tbody>
-          {getHeaderGroups().map((headerGroup) => (
-            <tr key={headerGroup.id}>
-              {headerGroup.headers.map((header) => (
-                <th key={header.id}>
-                  {header.isPlaceholder
-                    ? null
-                    : flexRender(
-                        header.column.columnDef.header,
-                        header.getContext()
-                      )}
-                </th>
-              ))}
-            </tr>
-          ))}
-        </tbody>
-        <tbody>
-          {getRowModel().rows.map((row) => (
-            <tr key={row.id}>
-              {row.getVisibleCells().map((cell) => (
-                <th key={cell.id} onClick={() => handleRowClick(cell)}>
-                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                </th>
-              ))}
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      {isLoading ? (
+        <p>loading...</p>
+      ) : (
+        <table>
+          <tbody>
+            {getHeaderGroups().map((headerGroup) => (
+              <tr key={headerGroup.id}>
+                {headerGroup.headers.map((header) => (
+                  <th key={header.id}>
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+          <tbody>
+            {getRowModel().rows.map((row) => (
+              <tr key={row.id}>
+                {row.getVisibleCells().map((cell) => (
+                  <th key={cell.id} onClick={() => handleRowClick(cell)}>
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+
       {isPagination && page && totalResults && setPage && (
         <Pagination page={page} setPage={setPage} totalResults={totalResults} />
       )}
